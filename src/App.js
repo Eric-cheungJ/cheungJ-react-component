f50954d8fa48f070611b7495c1d33d8061d0210a
zhang <1335091855@qq.com>
2019-04-01 14:24:28 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from "react";
+import { Button } from "./components";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button text={"hello, i am a button"} />
+      </div>
+    );
+  }
+}
+
+export default App;
