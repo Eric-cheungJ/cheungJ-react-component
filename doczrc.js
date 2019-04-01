846e077caa6266ddbd613047ffe7db30f0a80fcb
zhang <1335091855@qq.com>
2019-04-01 17:09:54 +0800

    add:文档

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+import { css } from "docz-plugin-css";
+
+export default {
+  title: "cheungJ-react-component",
+  description: "A Design UI library for React",
+  plugins: [
+    css({
+      preprocessor: "postcss"
+    })
+  ]
+};
